9a89f149de574b45e72c35f564421856f2c6d512
Matheus Henrique dos Santos <matheus.henrique200598@gmail.com>
2022-03-07 17:46:23 -0300

    first commit

---
+++ src/Campos.java
@@ -0,0 +1,68 @@
+
+package sistema.bancario;
+
+import java.util.ArrayList;
+
+public class Campos {
+    int num, tel;
+    double saldo, cpf;
+    String nome, cidade, estado;
+
+    public static ArrayList<Campos> vet = new ArrayList<>();
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(double cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getTel() {
+        return tel;
+    }
+
+    public void setTel(int tel) {
+        this.tel = tel;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+}
